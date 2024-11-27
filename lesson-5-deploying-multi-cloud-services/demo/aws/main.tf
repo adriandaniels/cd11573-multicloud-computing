a586219c441b929ef2bc8fd11ad2ce87417f85da
Adrian Daniels <adrian_daniels@msn.com>
2024-11-27 14:39:50 -0500

    New resource group

--- lesson-5-deploying-multi-cloud-services/demo/aws/main.tf
+++ lesson-5-deploying-multi-cloud-services/demo/aws/main.tf
@@ -69,13 +69,3 @@ resource "aws_route_table_association" "private" {
 }
 
 ##### Your code starts here #####
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.public[0].id
-
-  tags = {
-    Name = "HelloWorld-Travis"
-  }
-}
--- lesson-5-deploying-multi-cloud-services/exercise-1/starter/azure/main.tf
+++ lesson-5-deploying-multi-cloud-services/exercise-1/starter/azure/main.tf
@@ -4,39 +4,34 @@ data "azurerm_resource_group" "example" {
 
 ##### Your code starts here #####
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+data "azurerm_resource_group" "example" {
+  name = "Regroup_5lblJAAm_aQ8"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "linuxfunctionappsa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = "functionsapptestsa"
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "B1"
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
+resource "azurerm_function_app" "example" {
+  name                       = "test-azure-functions"
+  location                   = data.azurerm_resource_group.example.location
+  resource_group_name        = data.azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  service_plan_id            = azurerm_service_plan.example.id
-
-  site_config {}
 }
